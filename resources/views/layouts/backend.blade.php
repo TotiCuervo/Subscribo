e9a008ee930dc881dcabf8530460bec9e2697b20
Joseph Murphy <jmrphyii@memphis.edu>
2018-02-25 09:53:24 -0600

    hello

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -9,6 +9,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
+    <script src="https://js.stripe.com/v3/"></script>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
