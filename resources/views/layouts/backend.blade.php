45038877be7ca98c2093d7ab6d2bb5519f99f0a6
Joseph Murphy <jmrphyii@memphis.edu>
2018-02-25 10:43:30 -0600

    test

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -72,7 +72,45 @@
             </div>
         </div>
     </nav>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <a class="navbar-brand" href="#">Navbar</a>
 
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+            <!-- Right Side Of Navbar -->
+            <ul class="navbar-nav mr-auto mt-2">
+                <!-- Authentication Links -->
+                @guest
+                <li><a href="{{ route('login') }}">Login</a></li>
+                <li><a href="{{ route('register') }}">Register</a></li>
+                @else
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
+                            {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
+
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    Logout
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    {{ csrf_field() }}
+                                </form>
+                            </li>
+                        </ul>
+                    </li>
+                    @endguest
+            </ul>
+
+
+        </div>
+    </nav>
     @yield('content')
 </div>
 
