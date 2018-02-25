a27035baa460a9446f7c227c997274d27b24e56f
Joseph Murphy <jmrphyii@memphis.edu>
2018-02-24 22:42:26 -0600

    payments

---
+++ app/Http/Middleware/CheckSubscription.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class CheckSubscription
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if(Auth::check()){
+            if (Auth::user()->subscribed('main')) {
+                return true;
+            }else{
+                return redirect('/pay');
+            }
+        }else{
+            abort(403, 'Unauthorized action.');
+        }
+        return $next($request);
+    }
+}
